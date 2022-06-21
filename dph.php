8c9bf055be62ade596237f702ca5cce242ad354d
CalleyG <59815240+CalleyG@users.noreply.github.com>
2022-06-21 16:28:29 -0400

    Add files via upload
    This project exercise is taken from an online tutorial presented from Multimedia Tutorials (mmtuts) on his YouTube channel, required for ITWP 2750 - PHP.

---
+++ dph.php
@@ -0,0 +1,11 @@
+
+<?php
+
+
+         $server = "localhost";
+         $username = "garvinc518_calley";
+         $password = "Sam121aria$";
+         $db = "garvinc518_dphphpsearch";
+         
+         $conn = mysqli_connect($server, $username, $password, $db);
+?>
